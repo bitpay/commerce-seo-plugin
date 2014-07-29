5639b3965f607a3777ef8d64db82941dbccdcd3c
BitPay <bitpay@bitpay.bp>
2014-07-29 16:30:52 -0400

    removed commerce seo comment

--- lang/german/modules/payment/bitpay.php
+++ lang/german/modules/payment/bitpay.php
@@ -24,23 +24,6 @@
  * THE SOFTWARE.
  */
 
-/*-----------------------------------------------------------------
-* 	ID:						cash.php
-* 	Letzter Stand:			v2.3
-* 	zuletzt geaendert von:	cseoak
-* 	Datum:					2012/11/19
-*
-* 	Copyright (c) since 2010 commerce:SEO by Webdesign Erfurt
-* 	http://www.commerce-seo.de
-* ------------------------------------------------------------------
-* 	based on:
-* 	(c) 2000-2001 The Exchange Project  (earlier name of osCommerce)
-* 	(c) 2002-2003 osCommerce - www.oscommerce.com
-* 	(c) 2003     nextcommerce - www.nextcommerce.org
-* 	(c) 2005     xt:Commerce - www.xt-commerce.com
-* 	Released under the GNU General Public License
-* ---------------------------------------------------------------*/
-
 define('MODULE_PAYMENT_BITPAY_STATUS_TITLE', 'Enable BitPay Module');
 define('MODULE_PAYMENT_BITPAY_STATUS_DESC', 'Do you want to accept bitcoin?');
 define('MODULE_PAYMENT_BITPAY_APIKEY_TITLE', 'API Key');
