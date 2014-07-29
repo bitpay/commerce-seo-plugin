9171673f38d151f48fb3748ad7f07d28f9ccaefa
BitPay <bitpay@bitpay.bp>
2014-07-29 16:23:12 -0400

    brought up to date with oscommerce changes

--- lang/german/modules/payment/bitpay.php
+++ lang/german/modules/payment/bitpay.php
@@ -23,7 +23,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
- 
+
 /*-----------------------------------------------------------------
 * 	ID:						cash.php
 * 	Letzter Stand:			v2.3
